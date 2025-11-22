a40cc97a4a8e720bf0d26e677a42cad174776215
Nahuel Rojas <nahuelrojasboca1824@gmail.com>
2025-11-22 03:52:42 -0300

    fix: Corrección de ruta para creación de clientes en backend     - Actualizada la URL de creación de clientes en urls.py.     - Unificada la ruta POST /customers para evitar conflictos y errores 405.     - Ajustes menores en vistas relacionadas para asegurar el correcto enrutamiento.

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import SearchOrdersView, SearchCustomersView, CreateCustomerView, Up
 urlpatterns = [
     path('orders', SearchOrdersView.as_view(), name='search-orders'),
     path("customers", SearchCustomersView.as_view(), name="search-customers"),
-    path("customers", CreateCustomerView.as_view(), name="create-customer"),
+    path("customers/create", CreateCustomerView.as_view(), name="create-customer"),
     path("customers/<str:customerid>",
          UpdateCustomerView.as_view(), name="update-customer"),
 ]
